81ee22c135060f8b73ef327f9f4790df0f2cc63b
unknown <nhungnhuc1992@gmail.com>
2018-06-29 01:31:42 +0700

    Chi Nhung nhuc nop bai ve CODE

--- Session02/homework/Session02_Special.py
+++ Session02/homework/Session02_Special.py
@@ -4,32 +4,28 @@ rong = 19*n
 
 cao = 5*n
 
-# for i in range (1, cao+1):
-#     if i == 1 or i == 5*n:
-for j in range (1, rong+1):
-            if j == 5*n or j == 10*n or j == 15*n or j == 14*n:
-                print (" "*n, end = "")
+for i in range (1, cao+1):
+    if i == 1 or i == 5*n:
+        for j in range (1, rong+1):
+            if (4*n) < j <= (5*n) or (9*n) < j <= (10*n) or (13*n) < j < (15*n):
+                print (" ", end = "")
             else:
                 print ("*", end = "")
-print()
-    # elif i == 3*n:
-    #     for l in range (1, rong+1):
-    #         if l == 1 or l == (6*n) or l == (9*n) or l == (11*n) or l == (14*n) or l == (16*n) or l == (17*n) or l == (18*n) or l == (19*n):
-    #             print ("* ", end = "")
-    #         elif l%5 == 0 and l != (15*n):
-    #             print (" ", end = "")
-    #         elif l != (15*n):
-    #             print (" ", end = "")
-    #     print()
-    # else:
-    #     for k in range (1, rong+1):
-    #         if k == 1 or k == (6*n) or k == (9*n) or k == (11*n) or k == (14*n) or k == (16*n):
-    #             print ("* ", end = "")
-    #         elif k%5 == 0 and k != (15*n):
-    #             print (" ", end = "")
-    #         elif k != (15*n):
-    #             print (" ", end = "")
-    #     print()
+        print()
+    elif i == 3*n:
+        for m in range (1, rong+1):
+            if 1 <= m <=n or (5*n) < m <= (6*n) or (8*n) < m <= (9*n)  or (10*n) < m <= (11*n) or (13*n) < m <= (14*n) or (15*n) <= m <= (19*n):
+                print ("*", end = "")
+            else:
+                print (" ", end = "")
+        print()
+    else:
+        for k in range (1, rong+1):
+            if 1 <= k <=n or (5*n) < k <= (6*n) or (8*n) < k <= (9*n)  or (10*n) < k <= (11*n) or (13*n) < k <= (14*n) or (15*n) <= k < (16*n):
+                print ("*", end = "")
+            else:
+                print (" ", end = "")
+        print()
 
 
 
